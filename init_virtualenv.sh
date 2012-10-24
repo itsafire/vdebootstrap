3dc697a0921320b4e73600038ce9aac3d2ca5f32
itsafire <andreas.github@gomorrha.de>
2012-10-24 18:40:02 +0200

    [DEB:REMOVE] libzmq [DEB:ADD] rabbitmq-server pika tornado

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -6,9 +6,8 @@
 # create a virtualenv for this python project
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-apt-get -y -q install libzmq1 python-dev python-virtualenv
+apt-get -y -q install rabbitmq-server python-dev python-virtualenv
 virtualenv --no-site-packages $SCRIPT_DIR
 . $SCRIPT_DIR/bin/activate
 pip -q install plumbum
-pip -q install pyzmq
- 
+pip -q install pika
