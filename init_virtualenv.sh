f2c84cde7e59df40c0d01f7036ea464db712a6be
root <root@debian.localdomain>
2012-10-24 17:42:56 +0200

    [SETUP] installing zmq and python bindings, virtualenv

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -6,11 +6,9 @@
 # create a virtualenv for this python project
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "installing libzmql"
-apt-get -y -q install libzmq1
-apt-get -y -q install python-dev 
+apt-get -y -q install libzmq1 python-dev python-virtualenv
 virtualenv --no-site-packages $SCRIPT_DIR
 . $SCRIPT_DIR/bin/activate
-pip -q install blumbum
+pip -q install plumbum
 pip -q install pyzmq
  
